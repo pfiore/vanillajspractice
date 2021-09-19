87150af5f6d18287bdcd1a525cec677d427a94a5
pfiore <patfiorentino@gmail.com>
2021-09-19 12:37:56 -0700

    test 2

--- assets/scripts/vendor.js
+++ assets/scripts/vendor.js
@@ -5,7 +5,7 @@ const multiplyBtn = document.getElementById('btn-multiply');
 const divideBtn = document.getElementById('btn-divide');
 
 const currentResultOutput = document.getElementById('current-result');
-// const currentCalculationOutput = document.getElementById('current-calculation');
+const currentCalculationOutput = document.getElementById('current-calculation');
 
 function outputResult(result, text) {
   currentResultOutput.textContent = result;
