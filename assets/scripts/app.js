1629708c3c67e9c283c3fe3a7e4e6b96340051c8
pfiore <patfiorentino@gmail.com>
2021-09-23 16:09:11 -0700

    adds intial calculation logic

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,1 +1,54 @@
-alert('This Works!');
+const defaultResult = 0;
+let currentResult = defaultResult;
+
+
+
+function getUserInputNumber() {
+    return parseInt(userInput.value);
+}
+
+function writeLogOutput(operator, resultBeforeCalc, caclNumber) {
+    const enteredNumber = getUserInputNumber();
+    const calcDescription = `${currentResult} ${operator} ${enteredNumber}`;
+    outputResult(currentResult, calcDescription);
+
+}
+
+
+function add() {
+    const enteredNumber = getUserInputNumber();
+    const initialResult = currentResult;
+    currentResult = currentResult + enteredNumber;
+    writeLogOutput('+', initialResult, enteredNumber);
+}
+
+function subtract() {
+    const enteredNumber = getUserInputNumber();
+    const initialResult = currentResult;
+    currentResult = currentResult - enteredNumber;
+    writeLogOutput('-', initialResult, enteredNumber);
+}
+
+function multiply() {
+    const enteredNumber = getUserInputNumber();
+    const initialResult = currentResult;
+    currentResult = currentResult * enteredNumber;
+    writeLogOutput('*', initialResult, enteredNumber);
+}
+
+function divide() {
+    const enteredNumber = getUserInputNumber();
+    const initialResult = currentResult;
+    currentResult = currentResult / enteredNumber;
+    writeLogOutput('/', initialResult, enteredNumber);
+}
+
+
+
+
+
+addBtn.addEventListener('click', add);
+subtractBtn.addEventListener('click', subtract);
+multiplyBtn.addEventListener('click', multiply);
+divideBtn.addEventListener('click', divide);
+
